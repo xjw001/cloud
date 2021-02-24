9e074116e768b1bd04a808050e8944f373ad0e3b
xiongjw <xiongjianwen@hrfax.cn>
2021-02-24 20:02:16 +0800

    主程序

---
+++ cloud-provider-payment8001/src/main/java/com/xjw/springcloud/paymentMain8001.java
@@ -0,0 +1,11 @@
+package com.xjw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class paymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(paymentMain8001.class,args);
+    }
+}
