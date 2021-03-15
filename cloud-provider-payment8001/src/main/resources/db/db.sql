61b44b97c7334c59908f715d52198477a0e58479
xiongjw <xiongjianwen@hrfax.cn>
2021-03-15 09:47:49 +0800

    mysql数据库配置

---
+++ cloud-provider-payment8001/src/main/resources/db/db.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS `payment`;
+​
+CREATE TABLE `payment` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `serial` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+insert  into `payment`(`id`,`serial`) values (1,'尚硅谷'),(2,'alibaba'),(3,'京东'),(4,'头条');
