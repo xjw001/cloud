c536bdb8e8aee7269f05a214a564590b3dbd88a0
xjw001 <349770730@qq.com>
2021-06-17 01:17:51 +0800

    sentinel

--- cloud-alibaba-sentinel-service8401/src/main/java/com/xjw/springcloud/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service8401/src/main/java/com/xjw/springcloud/controller/FlowLimitController.java
@@ -1,4 +1,20 @@
 package com.xjw.springcloud.controller;
 
 public class FlowLimitController {
+
+    public void save(){
+
+    }
+
+    public static void main(String[] args) {
+        String str1 = new StringBuilder("58").append("tongcheng").toString();
+        System.out.println(str1);
+        System.out.println(str1.intern());
+        System.out.println(str1 == str1.intern());
+        System.out.println();
+        String str2 = new StringBuilder("ver").append("sion").toString();
+        System.out.println(str2);
+        System.out.println(str2.intern());
+        System.out.println(str2 == str2.intern());
+    }
 }
